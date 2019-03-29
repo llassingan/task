f02f860b80436e77efcecc6e4eba6c048f494d55
Nggak <45626951+llassingan@users.noreply.github.com>
2019-03-29 18:25:28 +0700

    Update shortest.cpp

--- TASK1/shortest.cpp
+++ TASK1/shortest.cpp
@@ -4,29 +4,17 @@ using namespace std;
 #define N 8 
 #define INF INT_MAX 
   
-//mengembalikan jarak terpendek dari node 0 sampai N-1. 
 int shortestDist(int graph[N][N]) { 
-  
-    // dist[i] AKAN MENYIMPAN NILAI TERPENDEK JARAK DARI NODE I KE NODE N-1 
     int dist[N]; 
   
     dist[N-1] = 0; 
-  
-    // menghitung rute terpendek dari sisa node yang lain
-    for (int i = N-2 ; i >= 0 ; i--) 
-    { 
-        // isi jarak dari i ke destinasi (N-1) 
+    for (int i = N-2 ; i >= 0 ; i--) { 
         dist[i] = INF; 
-  
-        // menyiapkan semua node dari tingkat selanjutnya untuk menemukan jarak terpendek dari i ke N-1. 
-        for (int j = i ; j < N ; j++) 
-        { 
-            // tolak jika tidak ada sisi tersisa
+      
+        for (int j = i ; j < N ; j++){ 
             if (graph[i][j] == INF) 
                 continue; 
   
-            // kita pake rekursif buat cari jarak ke target melalui j.
-            // dan membandingkan dengan jarak minimal sampai saat ini. 
             dist[i] = min(dist[i], graph[i][j] + dist[j]); 
         } 
     } 
@@ -35,7 +23,7 @@ int shortestDist(int graph[N][N]) {
    
 int main() 
 { 
-    // graph kita buat dalam matriks ketetanggaan sesuai soal 
+      //MATRIKS KETETANGGAN DARI GRAF SOAL
     int graph[N][N] = 
       {{INF, 6, 14, 10, INF, INF, INF, INF}, 
        {INF, INF, 6, INF, 24, INF, INF, INF}, 
